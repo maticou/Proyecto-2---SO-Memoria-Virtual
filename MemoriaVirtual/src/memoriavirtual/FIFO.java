8a7a3eb70cb8bca47d8739f53e987f0b75eec053
maticou <maticou123@gmail.com>
2019-05-30 18:35:57 -0400

    FIFO terminado

--- MemoriaVirtual/src/memoriavirtual/FIFO.java
+++ MemoriaVirtual/src/memoriavirtual/FIFO.java
@@ -13,8 +13,71 @@ import java.util.ArrayList;
  */
 public class FIFO {
 
+    ArrayList<Integer> procesos = new ArrayList<>();
+    ArrayList<Integer> insertos = new ArrayList<>();
+    ArrayList<String> paginas = new ArrayList<>(3);
+    ArrayList<Integer> colisiones = new ArrayList<>();
+    
     public FIFO(ArrayList<Integer> procesos) {
-        System.out.printf("Bienvenido a FIFO \n");
+        
+        this.procesos = procesos;  
+        this.paginas.add(" ");
+        this.paginas.add(" ");
+        this.paginas.add(" ");
+        simulador();
     }        
+
+    private void simulador() {
+        int contador = 0;
+        boolean a;
+        
+        for(int i=0; i<this.procesos.size(); i++){            
+            if(i>2){
+                if(this.paginas.contains(this.procesos.get(i))){
+                    a = false;
+                    imprimir(i, a);
+                }else{
+                    this.paginas.remove(0);
+                    this.paginas.add(2, Integer.toString(this.procesos.get(i)));
+                    this.insertos.add(this.procesos.get(i));
+                    a = true;
+                    imprimir(i, a);
+                }                 
+            }else{
+                if(this.paginas.contains(Integer.toString(this.procesos.get(i)))){
+                    a = false;
+                    imprimir(i, a);
+                }else{
+                    this.paginas.remove(i);
+                    this.paginas.add(contador, Integer.toString(this.procesos.get(i)));
+                    this.insertos.add(this.procesos.get(i));
+                    contador++;
+                    a = true;
+                    imprimir(i, a);
+                }                
+            }
+        }
+    }
+    
+    
+    private void imprimir (int i, boolean a){
+        
+        if(a){
+           System.out.println("\nEl proceso " + this.procesos.get(i) + " se agregó a memoria"); 
+        }
+        else{
+            System.out.println("El proceso " + this.procesos.get(i) + " ya está en memoria"); 
+        }      
+        for(int j = 0 ; j<this.paginas.size(); j++){
+            switch(j){
+                case 0: System.out.print("\n|" + this.paginas.get(j)); 
+                        break;
+                case 1: System.out.print("|" + this.paginas.get(j));  
+                        break;
+                case 2: System.out.println("|" + this.paginas.get(j) + "|");  
+                        break;
+            }                     
+        }        
+    }
     
 }
